bcc296a9c13b859f71523942498e46f5685e5ddc
Sol-J <80877740+Sol-J@users.noreply.github.com>
2022-10-17 20:35:14 +0900

    Update chart-pie-demo.js

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["주소", "서울", "집", "이사"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [60, 22, 11, 7],
+      backgroundColor: ['#F8CF69', '#FADB91', '#FBE4AC', '#FDF3DB'],
+      hoverBackgroundColor: ['#F8CF69', '#FADB91', '#FBE4AC', '#FDF3DB'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
