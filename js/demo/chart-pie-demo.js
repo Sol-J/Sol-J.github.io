29441ad0d633d83839110eddfa883f8244fd20b3
Sol-J <80877740+Sol-J@users.noreply.github.com>
2022-10-18 00:27:23 +0900

    Update chart-pie-demo.js

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,7 +7,7 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["주소 0.49", "서울 0.18", "집 0.09", "이사 0.02"],
+    labels: ["주소 (0.49)&nbsp;", "서울 (0.18)&nbsp;", "집 (0.09)&nbsp;", "이사 (0.02)&nbsp;"],
     datasets: [{
       data: [60, 22, 11, 7],
       backgroundColor: ['#F8CF69', '#FADB91', '#FBE4AC', '#FDF3DB'],
@@ -31,5 +31,4 @@ var myPieChart = new Chart(ctx, {
     cutoutPercentage: 40,
 
   },
-  title: "Weigths"
 });
