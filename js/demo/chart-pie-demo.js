cfc71905dde12b2f7a73f06cbe93a0a170179c52
Sol-J <80877740+Sol-J@users.noreply.github.com>
2022-11-02 02:16:51 +0900

    Update chart-pie-demo.js

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -37,3 +37,40 @@ var myPieChart = new Chart(ctx, {
 
   },
 });
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart2");
+var myPieChart2 = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["단어 (0.88) ", "오옝 (0.10) ", "아무 (0.01) ", "? (0.01)"],
+    datasets: [{
+      data: [60, 22, 11, 7],
+      backgroundColor: ['#F8CF69', '#FADB91', '#FBE4AC', '#FDF3DB'],
+      hoverBackgroundColor: ['#F8CF69', '#FADB91', '#FBE4AC', '#FDF3DB'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",      
+
+    }],
+  },
+  options: {
+    legend: {
+      position: 'right',
+      align: 'middle'
+    },
+    
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 10,
+      yPadding: 10,
+      displayColors: false,
+      caretPadding: 7,
+    },
+    cutoutPercentage: 80,
+
+  },
+});
+
