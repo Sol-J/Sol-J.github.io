95ddc91853d4e62e0eacf78bf330d741f64ea7b2
Sol-J <80877740+Sol-J@users.noreply.github.com>
2022-11-03 21:19:21 +0900

    Update chart-pie-demo.js

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -2,41 +2,7 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-// Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["주소 (0.49) ", "서울 (0.18) ", "집 (0.09) ", "이사 (0.02)"],
-    datasets: [{
-      data: [60, 22, 11, 7],
-      backgroundColor: ['#F8CF69', '#FADB91', '#FBE4AC', '#FDF3DB'],
-      hoverBackgroundColor: ['#F8CF69', '#FADB91', '#FBE4AC', '#FDF3DB'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",      
 
-    }],
-  },
-  options: {
-    legend: {
-      position: 'right',
-      align: 'middle'
-    },
-    
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 10,
-      yPadding: 10,
-      displayColors: false,
-      caretPadding: 7,
-    },
-    cutoutPercentage: 80,
-
-  },
-});
 
 var ctx1 = document.getElementById("myPieChart1");
 var myPieChart1 = new Chart(ctx1, {
