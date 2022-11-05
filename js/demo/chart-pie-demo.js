dbb59d37aa7f96d43b40b0a69bb1cd566d2b6067
Sol-J <80877740+Sol-J@users.noreply.github.com>
2022-11-05 17:06:21 +0900

    Update chart-pie-demo.js

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -1997,7 +1997,9 @@ var myPieChart50 = new Chart(ctx50, {
   },
 });
 
-
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
 
 // Pie Chart Example (51)
 var ctx51 = document.getElementById("myPieChart51");
