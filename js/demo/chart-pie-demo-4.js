c0a44f92d423b3e6bc57380632cafe44f9c92263
Sol-J <80877740+Sol-J@users.noreply.github.com>
2023-02-12 19:27:57 +0900

    Create chart-pie-demo-4.js

---
+++ js/demo/chart-pie-demo-4.js
@@ -0,0 +1,39 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+        
+// Pie Chart Example (1000)
+var ctx1000 = document.getElementById("myPieChart1000");
+var myPieChart1000 = new Chart(ctx1000, {
+  type: 'doughnut',
+  data: {
+    labels: ["getting (0.13) ", "COVID-19 (0.11) ", "risk (0.09) ", "e-cigarettes (0.07) "],
+    datasets: [{
+      data: [32.5, 27.5, 22.5, 17.5],
+      backgroundColor: ['#2DB7CA', '#5CC8D6', '#8AD6E2', '#C8ECF2'],
+      hoverBackgroundColor: ['#2DB7CA', '#5CC8D6', '#8AD6E2', '#C8ECF2'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",      
+
+    }],
+  },
+  options: {
+    legend: {
+      position: 'right',
+      align: 'middle'
+    },
+    
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 10,
+      yPadding: 10,
+      displayColors: false,
+      caretPadding: 7,
+    },
+    cutoutPercentage: 80,
+
+  },
+});
